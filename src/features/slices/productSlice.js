1a85f848b5f455fd2080d83edf34e5d0c0b8c127
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-10-01 12:52:53 +0200

    Setting up components

---
+++ src/features/slices/productSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+  name: "products",
+  initialState: {
+    loading: null,
+    data: null,
+  },
+
+  reducers: {
+    setData: (state, action) => {
+      state.data = action.payload;
+      state.loading = null;
+    },
+  },
+});
+
+export const { setData } = productSlice.actions;
+
+export default productSlice.reducer;
